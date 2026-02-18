e36916873a2afa0faf5537d3ca11f4d2ad0d1f59
Ashni-08 <boseashni72@gmail.com>
2026-02-18 22:27:45 +0530

    Login changes

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "./Style.css";
@@ -8,10 +7,7 @@ function Login() {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-
   const [showPassword, setShowPassword] = useState(false);
-
 
   const navigate = useNavigate();
 
@@ -24,7 +20,7 @@ function Login() {
       user => user.username === username && user.password === password
     );
 
-    if(validUser){
+    if (validUser) {
       localStorage.setItem("loggedUser", JSON.stringify(validUser));
       alert("Login Successful");
       navigate("/");
@@ -44,7 +40,7 @@ function Login() {
             type="text"
             placeholder="Username"
             value={username}
-            onChange={(e)=>setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
 
@@ -54,7 +50,7 @@ function Login() {
               type={showPassword ? "text" : "password"}
               placeholder="Password"
               value={password}
-              onChange={(e)=>setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
 
@@ -77,7 +73,6 @@ function Login() {
         </form>
 
         <p style={{ marginTop: "15px", textAlign: "center" }}>
-
           New User?{" "}
           <Link
             to="/register"
@@ -85,9 +80,6 @@ function Login() {
           >
             Register Here
           </Link>
-
-          New User? <Link to="/register">Register Here</Link>
-
         </p>
 
       </div>
